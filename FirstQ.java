0bba94b37824a36088a135abb7979eca977f8617
Corvo000 <55488071+Corvo000@users.noreply.github.com>
2020-06-18 12:14:48 +0200

    Create FirstQ.java

---
+++ FirstQ.java
@@ -0,0 +1,44 @@
+public class FirstQ {
+
+	public static void main(String[] args) 
+	{
+		QueueViaArray<Integer> q1 = new QueueViaArray<Integer>(); 
+		q1.put(1);
+		System.out.println(q1);
+		q1.put(2);
+		System.out.println(q1);
+		q1.put(3);
+		System.out.println(q1);
+		q1.put(4);
+		System.out.println(q1);
+		q1.put(5);
+		System.out.println(q1);
+		q1.put(6);
+		System.out.println(q1);
+		q1.put(7);
+		System.out.println(q1);
+		q1.put(8);
+		System.out.println(q1);
+		q1.put(9);
+		System.out.println(q1);
+		q1.get();
+		System.out.println(q1);
+		q1.get();
+		System.out.println(q1);
+		q1.get();
+		System.out.println(q1);
+		q1.get();
+		System.out.println(q1);
+		q1.get();
+		System.out.println(q1);
+		q1.get();
+		System.out.println(q1);
+		q1.get();
+		System.out.println(q1);
+		q1.get();
+		System.out.println(q1);
+		q1.get();
+		System.out.println(q1);
+	}
+
+}
